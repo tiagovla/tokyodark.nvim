903c73f5663bc1e955310dc55618d35a5965e32d
tiagovla <tiagovla@gmail.com>
2021-10-23 01:51:38 -0300

    feat: expose colors

--- lua/lualine/themes/tokyodark.lua
+++ lua/lualine/themes/tokyodark.lua
@@ -1,7 +1,7 @@
 -- Copyright (c) 2021-2021 tiagovla
 -- License: MIT
 --
-local p = require('tokyodark.palette')
+local p = require("tokyodark.palette")
 local colors = {
     bg = p.bg0,
     fg = p.fg,
@@ -11,25 +11,34 @@ local colors = {
     blue = p.blue,
     purple = p.purple,
     cyan = p.cyan,
-    gray = p.grey
+    grey = p.grey,
 }
 
-local tokyo_night = {
+local tokyo_dark = {
     inactive = {
-        a = {fg = colors.gray, bg = colors.bg, gui = 'bold'},
-        b = {fg = colors.gray, bg = colors.bg},
-        c = {fg = colors.bg, bg = colors.bg},
-        z = {fg = colors.gray, bg = colors.bg}
+        a = { fg = colors.grey, bg = colors.bg, gui = "bold" },
+        b = { fg = colors.grey, bg = colors.bg },
+        c = { fg = colors.grey, bg = colors.bg },
+        z = { fg = colors.grey, bg = colors.bg },
     },
     normal = {
-        a = {fg = colors.bg, bg = colors.green, gui = 'bold'},
-        b = {fg = colors.fg, bg = colors.bg},
-        c = {fg = colors.fg, bg = colors.bg},
-        z = {fg = colors.fg, bg = colors.bg}
+        a = { fg = colors.bg, bg = colors.green, gui = "bold" },
+        b = { fg = colors.fg, bg = colors.bg },
+        c = { fg = colors.fg, bg = colors.bg },
+        z = { fg = colors.fg, bg = colors.bg },
     },
-    visual = {a = {fg = colors.bg, bg = colors.purple, gui = 'bold'}},
-    replace = {a = {fg = colors.bg, bg = colors.red, gui = 'bold'}},
-    insert = {a = {fg = colors.bg, bg = colors.blue, gui = 'bold'}}
+    visual = { a = { fg = colors.bg, bg = colors.purple, gui = "bold" } },
+    replace = { a = { fg = colors.bg, bg = colors.red, gui = "bold" } },
+    insert = { a = { fg = colors.bg, bg = colors.blue, gui = "bold" } },
 }
 
-return tokyo_night
+local mt = {}
+function mt:__index(k)
+    if k == "colors" then
+        return colors
+    end
+end
+
+tokyo_dark = setmetatable(tokyo_dark, mt)
+
+return tokyo_dark
