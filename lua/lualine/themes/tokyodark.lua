78a225ba2fe7a6d8d4f12dee2e7bdc1fedcd75d9
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-03-13 18:54:35 +0530

    fix(#33): add command mode color for lualine (#35)

--- lua/lualine/themes/tokyodark.lua
+++ lua/lualine/themes/tokyodark.lua
@@ -34,6 +34,7 @@ local tokyo_dark = {
     visual = { a = { fg = colors.bg, bg = colors.purple, gui = "bold" } },
     replace = { a = { fg = colors.bg, bg = colors.red, gui = "bold" } },
     insert = { a = { fg = colors.bg, bg = colors.blue, gui = "bold" } },
+    command = { a = { fg = colors.bg, bg = colors.yellow, gui = "bold" } },
 }
 
 local mt = {}
